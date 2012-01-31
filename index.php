82fecc402a7aea2d21df24e8c158bbb778be2aa8
Paul Ridgway <myself@paulridgway.co.uk>
2012-01-31 15:01:48 +0000

    x

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<a href="#">Test</a>
-<?php phpinfo(); ?>
+
+<a href="/">PHP Info</a>
+|
+<a href="/?cpu=1">PHP Info</a>
+<?php 
+	if ($_REQUEST['cpu']) {
+		echo "Run CPU"
+		for($i = 0; i < 1000000000; ++$i) {
+			
+		}
+		echo "CPU Ran"
+		
+	} else {
+    	phpinfo();
+	}
+?>
