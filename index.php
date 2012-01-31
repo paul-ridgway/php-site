4a4229e6c9c65a67496e58b5be36e73aacae9e48
Paul Ridgway <myself@paulridgway.co.uk>
2012-01-31 15:16:28 +0000

    x

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
 				$time = microtime();
 				$time = explode(' ', $time);
 				$time = $time[1] + $time[0];
-                echo "Run CPU...<br />";
+				$start = $time;                echo "Run CPU...<br />";
                 for($i = 0; $i < 100000000; ++$i) {
 
                 }
                 echo "CPU Ran";
+				$time = microtime();
+				$time = explode(' ', $time);
+				$time = $time[1] + $time[0];
 				$finish = $time;
 				$total_time = round(($finish - $start), 4);
-				echo ' in '.$total_time.' seconds.'."\n";
+				echo ' in '. $total_time.' seconds.'."\n";
 
         } else {
                 phpinfo();
